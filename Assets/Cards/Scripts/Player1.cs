5b07f70690ed9052b116fdefa66a58de9bc3294a
KIT4276 <kit311290@gmail.com>
2022-09-08 15:10:27 +0300

    Done: Hero picture

--- Assets/Cards/Scripts/Player1.cs
+++ Assets/Cards/Scripts/Player1.cs
@@ -6,7 +6,6 @@ namespace Cards
 {
     public class Player1 : PlayerManager
     {
-        
         private void Awake()
         {
             _sideType = Panel1.Self.GetSideType();
--- Assets/Cards/Scripts/PlayerManager.cs
+++ Assets/Cards/Scripts/PlayerManager.cs
@@ -8,16 +8,46 @@ namespace Cards
     {
         protected int _halth = 20;
 
+        
         [SerializeField]
         protected TextMeshPro _halthIndicator;
         [SerializeField]
         protected TextMeshPro _sideTypeText;
+
+        [Space, Tooltip("All class materials"), SerializeField]
+        protected Material _MageMaterial;
+        [SerializeField]
+        protected Material _WarriorMterial;
+        [SerializeField]
+        protected Material _PriestMterial;
+        [SerializeField]
+        protected Material _HunterMterial;
 
         protected SideType _sideType;
 
         protected void Start()
         {
             _sideTypeText.text = _sideType.ToString();
+            SetMaterial();
+        }
+
+        protected void SetMaterial() 
+        {
+            switch (_sideType)
+            {
+                case SideType.Mage:
+                    gameObject.GetComponent<MeshRenderer>().material = _MageMaterial;
+                    break;
+                case SideType.Warrior:
+                    gameObject.GetComponent<MeshRenderer>().material = _WarriorMterial;
+                    break;
+                case SideType.Priest:
+                    gameObject.GetComponent<MeshRenderer>().material = _PriestMterial;
+                    break;
+                case SideType.Hunter:
+                    gameObject.GetComponent<MeshRenderer>().material = _HunterMterial;
+                    break;
+            }
         }
 
         public void SetHalth(int value)
